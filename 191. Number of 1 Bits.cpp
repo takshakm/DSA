3294af00ab9393d09e1b5ea18925c141f26a6582
Takshak Mudgal <takshakmudgal@gmail.com>
2022-10-20 19:06:01 +0530

    DSA

---
+++ 191. Number of 1 Bits.cpp
@@ -0,0 +1,15 @@
+//Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        int count=0;
+        while(n!=0) {
+            if(n&1) {
+                count++;
+            }
+            n = n>>1;            
+        }
+        return count;
+    }
+};
