1b6e02862e6a96b0aac33aeb18af204be0cdb366
Takshak Mudgal <114814639+takshakm@users.noreply.github.com>
2022-11-01 21:54:08 +0530

    submitted

--- 1281. Subtract the Product and Sum of Digits of an Integer.cpp
+++ 1281. Subtract the Product and Sum of Digits of an Integer.cpp
@@ -1,16 +1,19 @@
 //Given an integer number n, return the difference between the product of its digits and the sum of its digits.
-class Solution {
-public:
-    int subtractProductAndSum(int n) {
-        int prod=1;
-        int sum=0;
-        while(n!=0){
-            int rem =n%10;
-            prod = prod * rem;
-            sum = sum + rem;
-            n=n/10;
-        }
-        int result = prod - sum;
-        return result;
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum,prod,diff;
+    cout<<"enter a number ";
+    cin>>n;
+    sum=0;
+    prod=1;
+    while(n!=0){
+        int rem=n%10;
+        prod=prod*rem;
+        sum=sum+rem;
+        n=n/10;
     }
-};
+    diff=prod-sum;
+    cout<< diff<<endl;
+    return 0;
+}
